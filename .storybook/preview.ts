e877067c75f7d63af78e20a80d83a809bd31f55e
Nicolau Barsotti <nicolau.barsotti@birdirx.com>
2024-12-27 14:29:35 -0300

    build: add support for styled-components theme in storybook files

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,16 @@ import type { Preview } from "@storybook/react";
 
 import "../src/App.css";
 
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import { theme } from "../src/config/theme";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Runescape";
+  }
+`;
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -11,6 +21,17 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      themes: {
+        light: theme,
+      },
+      defaultTheme: "light",
+      Provider: ThemeProvider,
+      GlobalStyles,
+    }),
+  ],
 };
 
 export default preview;
