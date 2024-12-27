921d0efdc114ad1b7c97a69ce9ef4759decceaba
Nicolau Barsotti <nicolau.barsotti@birdirx.com>
2024-12-27 19:17:40 -0300

    build: add react-router to the project

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router";
 import { theme } from "./config/theme.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
