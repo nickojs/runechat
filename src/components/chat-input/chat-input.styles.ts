d7369743d863becdd283b85f01e02636fdfe31c0
Nicolau Barsotti <nicolau.barsotti@birdirx.com>
2024-12-27 15:23:31 -0300

    feat: create send button for chat input component

--- src/components/chat-input/chat-input.styles.ts
+++ src/components/chat-input/chat-input.styles.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import sendImg from "../../assets/img/send.png";
 
 const ChatInputContainer = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
 
   padding: 16px;
 
@@ -25,4 +27,53 @@ const InputOverride = styled.input`
   background-color: transparent;
 `;
 
-export { ChatInputContainer, InputOverride };
+const SendBtn = styled.button`
+  all: unset;
+  position: relative;
+
+  margin-left: 8px;
+  width: 30px;
+  height: 30px;
+
+  cursor: pointer;
+  user-select: none;
+
+  color: transparent;
+
+  background-image: url(${sendImg});
+  background-size: 100%;
+
+  &:active {
+    background-size: 105%;
+    transform: translateY(-5%);
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    pointer-events: none;
+
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      clip-path: polygon(
+        50% 0%,
+        80% 10%,
+        100% 35%,
+        100% 70%,
+        80% 90%,
+        50% 100%,
+        20% 90%,
+        0% 70%,
+        0% 35%,
+        20% 10%
+      );
+    }
+  }
+`;
+
+export { ChatInputContainer, InputOverride, SendBtn };
--- src/components/chat-input/index.tsx
+++ src/components/chat-input/index.tsx
@@ -1,10 +1,21 @@
-import { ChatInputContainer, InputOverride } from "./chat-input.styles";
+import React, { useState } from "react";
+import {
+  ChatInputContainer,
+  InputOverride,
+  SendBtn,
+} from "./chat-input.styles";
 
 const ChatInput = () => {
+  const [input, setInput] = useState<string>("");
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInput(e.target.value);
+
   return (
     <ChatInputContainer>
       <p>username:</p>
-      <InputOverride />
+      <InputOverride onChange={inputHandler} />
+      <SendBtn disabled={!input}>Send</SendBtn>
     </ChatInputContainer>
   );
 };
