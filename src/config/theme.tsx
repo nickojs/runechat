f0232a39737092a7ba296c4f39daf7fe0efaa9f8
Nicolau Barsotti <nicolau.barsotti@birdirx.com>
2024-12-27 14:29:07 -0300

    feat: create theme object

---
+++ src/config/theme.tsx
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    bgPrimary: "#483e32",
+    bgDarker: "#3b332b",
+    bgLight: "#ccbb9a",
+    text: "#ffffff",
+    textSecondary: "#000000",
+    title: "#ffb21f",
+  },
+  borderRadius: "8px",
+};
