ccb4e63cc8d9b77842a775e04db81e25835d225c
Nicolau Barsotti <nicolau.barsotti@birdirx.com>
2024-12-27 22:53:20 -0300

    feat: expand default button's disabled styles

--- src/components/ui/button/button.styles.ts
+++ src/components/ui/button/button.styles.ts
@@ -9,6 +9,16 @@ const buttonBase = css`
 
   &:disabled {
     cursor: not-allowed;
+
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+    }
   }
 `;
 
