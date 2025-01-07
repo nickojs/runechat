bbec45997e81535cf40474dfdff857a50c207e87
Nicolau Barsotti <nicolau.barsotti@birdirx.com>
2025-01-07 20:21:10 -0300

    feat: create auth selectors and use them in authLayout

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -1,7 +1,23 @@
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router";
+
+// import storageHelper from "../helpers/storage";
+import { tokenSelector } from "../store/auth/auth.selectors";
 import { AuthLayoutBase } from "./base.styles";
 
 const AuthLayout = () => {
+  let navigate = useNavigate();
+  // const token = storageHelper.auth.getToken(); -- imlpement it later for dev purposes
+  const token = useSelector(tokenSelector);
+
+  useEffect(() => {
+    if (token) {
+      console.log(token);
+      navigate("/main");
+    }
+  }, [token]);
+
   return (
     <AuthLayoutBase>
       <Outlet />
---
+++ src/store/auth/auth.selectors.ts
@@ -0,0 +1,12 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { RootState } from "..";
+
+export const authStore = (state: RootState) => state.authReducer;
+
+export const usernameSelector = createSelector(authStore, (store) => {
+  return store.username;
+});
+
+export const tokenSelector = createSelector(authStore, (store) => {
+  return store.authToken;
+});
